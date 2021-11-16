6d8e17840c7b962b1c0081468ae92c848599c8be
Abigail1222 <63478572+Abigail1222@users.noreply.github.com>
2021-11-15 19:20:00 -0600

    Ionic import

--- src/app/card-producto/card-producto.module.ts
+++ src/app/card-producto/card-producto.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardProductoComponent } from './card-producto.component';
+import { IonicModule } from '@ionic/angular';
 
 
 
