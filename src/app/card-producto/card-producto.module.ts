49ddf15bff866e9b5b925a8659ed894f3d690830
Abigail1222 <63478572+Abigail1222@users.noreply.github.com>
2021-11-15 19:40:00 -0600

    card try again

--- src/app/card-producto/card-producto.module.ts
+++ src/app/card-producto/card-producto.module.ts
@@ -3,13 +3,12 @@ import { CommonModule } from '@angular/common';
 import { CardProductoComponent } from './card-producto.component';
 import { IonicModule } from '@ionic/angular';
 
-
-
 @NgModule({
   declarations: [CardProductoComponent],
   exports: [CardProductoComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
   ]
 })
 export class CardProductoModule { }
